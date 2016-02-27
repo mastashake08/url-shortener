b1dd9e1dc112d8a5cf20ade12fd436b316f157a0
Jyrone Parker <jyrone.parker@gmail.com>
2016-02-27 20:43:48 +0000

    Added UrlController with addUrl method

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Url;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class UrlController extends Controller
+{
+    //
+    public function addUrl(Request $request){
+      $url = Url::Create([
+        'input_url' => $request->input_url,
+        'output_url' => str_random(8),
+      ]);
+      $request->session()->flash('url', $url->output_url);
+      return back();
+    }
+}
